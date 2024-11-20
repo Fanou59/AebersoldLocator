f06dad1a9e23ede39576e0ac9fdb7a900a85ca9d
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2024-11-20 14:32:52 +0100

    Update nelmio_cors.yaml configuration

--- deploy.php
+++ deploy.php
@@ -4,24 +4,25 @@ namespace Deployer;
 
 require 'recipe/symfony.php';
 
-// Config
+// Configuration Git
+set('repository', 'git@github.com:Fanou59/AebersoldLocator.git');
+set('branch', 'main'); // Remplacez par la branche que vous souhaitez déployer (par défaut, 'main').
 
-set('repository', 'https://github.com/Fanou59/AebersoldLocator.git');
-
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
-set('writable_mode', 'chmod'); // Utiliser chmod au lieu de setfacl
-set('writable_use_sudo', false); // Pas besoin de sudo sur Alwaysdata
-set('http_user', 'aebersoldlocator');
-
-// Hosts
-
-host('ssh-aebersoldlocator.alwaysdata.net')
+// Configuration serveur
+host('production')
+    ->setHostname('ssh-aebersoldlocator.alwaysdata.net')
     ->set('remote_user', 'aebersoldlocator')
     ->set('deploy_path', '~/www');
 
+set('http_user', 'aebersoldlocator');
 
-// Hooks
+// Fichiers et répertoires partagés
+set('shared_files', ['.env']);
+set('shared_dirs', ['var/log', 'var/sessions']);
+set('writable_mode', 'chmod'); // Utiliser chmod au lieu de setfacl
+set('writable_dirs', ['var', 'var/cache', 'var/log', 'var/sessions']);
+
+// Nombre de releases à conserver
+set('keep_releases', 3);
 
 after('deploy:failed', 'deploy:unlock');
